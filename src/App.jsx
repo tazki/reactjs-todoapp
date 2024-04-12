5b0f0eda646d5fac26c5a3cd609e160b0deb5f98
Mark Bautista <42196376+tazki@users.noreply.github.com>
2024-04-12 21:09:27 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,23 @@
+import styles from "./todoitem.module.css";
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
+}
