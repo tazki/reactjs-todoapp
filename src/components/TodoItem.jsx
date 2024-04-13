d1f1d8da3995bd2d62f039526f1e78e83f44d679
Mark Bautista <42196376+tazki@users.noreply.github.com>
2024-04-13 10:47:35 +0800

    [Add] Completed Action
    - Add marking todo item as completed
    - Add counting of  total todos and completed todos
    - Add sorting of todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,25 @@
 import styles from "./todoitem.module.css";
 export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
-    setTodos(todos.filter((todo) => todo !== item));
+    setTodos(todos.filter((todo) => todo.name !== item));
   }
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+  const className = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span onClick={() => handleClick(item.name)} className={className}>
+          {item.name}
+        </span>
         <span>
           <button
-            onClick={() => handleDelete(item)}
+            onClick={() => handleDelete(item.name)}
             className={styles.deleteButton}
           >
             x
